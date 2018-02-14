aa74cac4da0485a9473014ff8bb2ce2d779aefb5
ORWELLG\n.pimley <n.pimley@orwellg.com>
2018-02-14 09:46:50 +0000

    DB project moved to own repository

--- pharmacy.database/dbo/Tables/Note.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Note] (
-    [NoteID]    UNIQUEIDENTIFIER NOT NULL,
-    [Text]      NVARCHAR (MAX)   NULL,
-    [CreatedOn] DATETIME         NOT NULL,
-    CONSTRAINT [PK_dbo.Note] PRIMARY KEY CLUSTERED ([NoteID] ASC)
-);
-
--- pharmacy.database/dbo/Tables/Reminder.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Reminder] (
-    [ReminderID] UNIQUEIDENTIFIER NOT NULL,
-    [CustomerID] UNIQUEIDENTIFIER NOT NULL,
-    [SendTime]   DATETIME         NOT NULL,
-    [Sent]       BIT              DEFAULT ((0)) NOT NULL,
-    [CreatedOn]  DATETIME         NOT NULL,
-    CONSTRAINT [PK_reminder] PRIMARY KEY CLUSTERED ([ReminderID] ASC)
-);
-
